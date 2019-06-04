70c91d6915d2181733b60c5b4334ad6d7f88a483
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-04 22:45:02 +0000

    Deploy matheusdc/nyt-best-sellers to github.com/matheusdc/nyt-best-sellers.git:gh-pages

---
+++ precache-manifest.bf598ea9d4dcae81f7a47cab51fd8e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10d67b2e01ed25d047d989e50cde610",
+    "url": "/nyt-best-sellers/index.html"
+  },
+  {
+    "revision": "e7d2808bcd548941fdb0",
+    "url": "/nyt-best-sellers/static/css/2.17ef8162.chunk.css"
+  },
+  {
+    "revision": "a4b7f214b6d0c6454259",
+    "url": "/nyt-best-sellers/static/css/main.18e27db4.chunk.css"
+  },
+  {
+    "revision": "e7d2808bcd548941fdb0",
+    "url": "/nyt-best-sellers/static/js/2.cd9299f0.chunk.js"
+  },
+  {
+    "revision": "a4b7f214b6d0c6454259",
+    "url": "/nyt-best-sellers/static/js/main.25f79116.chunk.js"
+  },
+  {
+    "revision": "428ace7860a6d9408d0f",
+    "url": "/nyt-best-sellers/static/js/runtime~main.b7b4ff2b.js"
+  }
+]);
